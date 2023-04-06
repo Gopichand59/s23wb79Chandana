dd5eb7465c027ffe23e9e78fb6ec7f09e66be04a
Gopichand59 <s559979@nwmissouri.edu>
2023-04-05 21:30:05 -0500

    added selector files

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var JellyBellyRouter = require('./routes/JellyBelly');
 var boardRouter = require('./routes/board');
-
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -25,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/JellyBelly', JellyBellyRouter);
 app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
